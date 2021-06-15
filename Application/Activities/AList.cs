13d24e2d87ae89d8d23ca71336dca93efdeb1b07
baruasgrps <baruasgrps@gmail.com>
2021-06-16 01:13:05 +0600

    End of Section 5

--- Application/Activities/AList.cs
+++ Application/Activities/AList.cs
@@ -28,19 +28,19 @@ namespace Application.Activities
             }
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                try
-                {
-                    for (var i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }
-                catch (Exception ex) when (ex is TaskCanceledException)
-                {
-                    _logger.LogInformation($"Task was cancelled");
-                }
+                //try
+                //{
+                //    for (var i = 0; i < 10; i++)
+                //    {
+                //        cancellationToken.ThrowIfCancellationRequested();
+                //        await Task.Delay(1000, cancellationToken);
+                //        _logger.LogInformation($"Task {i} has completed");
+                //    }
+                //}
+                //catch (Exception ex) when (ex is TaskCanceledException)
+                //{
+                //    _logger.LogInformation($"Task was cancelled");
+                //}
                 return await _dataContext.Activities.ToListAsync(cancellationToken);
             }
         }
